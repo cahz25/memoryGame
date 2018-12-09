231a323e38a5f38d10ce780e5bbd3998711cbd3e
Cesar  Hurtado <cahz25@Cesars-MacBook-Pro.local>
2018-12-09 16:42:16 -0500

    Se organiza repositorio

--- memoryGame.playground/Contents.swift
+++ memoryGame.playground/Contents.swift
@@ -4,18 +4,21 @@ import UIKit
 
 for number in 0...100 {
     
+    //Se valida si el número es divisile por 5
     if number % 5 == 0 {
         print("\(number) Bingo!!!")
     }
     
+    //Se valida si el número es para o impar
     if number % 2 == 0 {
         print("\(number) par!!!")
     } else {
         print("\(number) impar!!!")
     }
     
+    //Se valida si el número esta en el rango de 30 a 40
     if number >= 30 && number <= 40 {
         print("\(number) Viva Swift!!!")
     }
-    
 }
+
